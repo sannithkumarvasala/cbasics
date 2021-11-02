0e3f1a8009d116ec1d76b662223889360bd6bb50
sannithkumarvasala <93171155+sannithkumarvasala@users.noreply.github.com>
2021-11-02 12:21:21 +0530

    Add files via upload

---
+++ natural sannith.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	
+	a = 5;
+	c = 10;
+	b = a*(a+1)/2;
+	d = c*(c+1)/2;
+	printf("Sum of first %d natural numbers is: %d",a, b);
+	printf("\nSum of first %d natural numbers is: %d",c,d);
+	
+}
