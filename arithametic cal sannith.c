979e0881100ab141882822eaee959d65250d6615
sannithkumarvasala <93171155+sannithkumarvasala@users.noreply.github.com>
2021-11-03 11:23:26 +0530

    Add files via upload

---
+++ arithametic cal sannith.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+     int a, b, c;
+     a = 2;
+     b = a*a;
+     c = 4*a;
+     printf("Area of the square with side %d is: %d\nperimeter of the square with side %d is:%d", a, b, a, c);
+
+}
