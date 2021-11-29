2f22cc0a33e5f1f167c074b0532fd2dc2a08c446
SANNITH KUMAR VASALA <93171155+sannithkumarvasala@users.noreply.github.com>
2021-11-29 12:13:39 +0530

    Add files via upload

---
+++ assign values.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	int a, b, c;
+	printf("INPUT:\n");
+	scanf("%d %d %d", &a, &b, &c);
+	printf("OUTPUT:\n");
+a += b;
+	printf("%d %d %d\n", a, b, c);
+b -= c;
+	printf("%d %d %d\n", a, b, c);
+c *= a;
+	printf("%d %d %d\n", a, b, c);
+a /= b;
+	printf("%d %d %d\n", a, b, c);
+b *= c;
+	printf("%d %d %d\n", a, b, c);
+a += b;
+	printf("%d %d %d\n", a, b, c);
+	
+	
+	
+	
+}
